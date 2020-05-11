5b784d7eb4949894044d14cac9ac332dad6ce294
Helio Gazzia <hgazzia@hotmail.com>
2020-05-11 16:32:29 -0300

    Update catalogar.py

--- catalogar.py
+++ catalogar.py
@@ -1,37 +1,28 @@
+""" Versión 2.0
+
+        No almacena cantidad de archivos que comienzan con cada caracter.
+        Cada subcarpeta se denomina con los x primeros caracteres del 
+        primer archivo y los x primeros del último.
+"""
+
 import os
 
-# empieza_con = []
-# cantidad_por_letra = []
 cantidad_de_carpetas = 0
 
 def catalogar(basepath):
-    # indice = -1
     archivos = []
     subcarpeta = []
     global cantidad_de_carpetas
-    # global cantidad_por_letra, empieza_con
 
     for dirpath, dirnames, files in os.walk(basepath):
         for carp in dirnames:
             subcarpeta.append(carp)
             cantidad_de_carpetas += 1
         for file_name in files:
-            # Chequear si la letra inicial cambió (mayúsculas = minúsculas)
-        # if not empieza_con or empieza_con[indice].casefold() \
-        #          != file_name[0].casefold():
-        #    indice += 1
-        #     cantidad_por_letra.append(0)
-        #    empieza_con.append(file_name[0])
             archivos.append(file_name)
-    #            cantidad_por_letra[indice] += 1
-    # try:
-    #    f = open("Siloplist.txt", "w")
-    #    l1 = map(lambda x:x+"\n", archivos)
-    #    f.writelines(l1)
-    #    f.close()
-    # except:
-    #    pass
     return archivos
+
+# Versión más moderna, pero extremadamente lenta.
 
 def catalogar2(directorio):
     global archivos, cantidad_de_carpetas
